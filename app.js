eb38387a0d642a9d9bbf1f8f5f56e4b9aa145da6
Gareth-Walters <gareth.a.walters@gmail.com>
2020-10-14 17:02:26 +0100

    Implemented getting and populating items into UI

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const ItemCtrl = (function () {
 
   // Public methods
   return {
+    getItems: function () {
+      return data.items;
+    },
     logData: function () {
       return data;
     },
@@ -35,8 +38,24 @@ const ItemCtrl = (function () {
 
 // UI Controller
 const UICtrl = (function () {
+  const UISelectors = {
+    itemList: '#item-list',
+  };
   //Public methods
-  return {};
+  return {
+    populateItemList: function (items) {
+      let html = '';
+      items.forEach((item) => {
+        html += `
+        <li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+        </li>`;
+      });
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    },
+  };
 })();
 
 //
@@ -47,7 +66,11 @@ const App = (function (ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function () {
-      console.log('Initialising App...');
+      // Get items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     },
   };
 })(ItemCtrl, UICtrl);
